eabb8c8c148b6a5d936044160b9b071be22e80e0
geoffrey jost <geoffrey@jostwebwerks.com>
2017-06-05 17:46:47 -0700

    config: provide guidance to user when databoxes config is off

--- encyc/config.py
+++ encyc/config.py
@@ -46,10 +46,13 @@ try:
 except:
     MEDIAWIKI_API_HTPASS = None
 MEDIAWIKI_API_TIMEOUT = config.get('mediawiki', 'api_timeout')
-MEDIAWIKI_DATABOXES = {
-    keyval.split(':')[0]: keyval.split(':')[1]
-    for keyval in config.get('mediawiki', 'databoxes').split(';')
-}
+try:
+    MEDIAWIKI_DATABOXES = {
+        keyval.split(':')[0]: keyval.split(':')[1]
+        for keyval in config.get('mediawiki', 'databoxes').split(';')
+    }
+except:
+    raise Exception('mediawiki.databox format: "MWDIVID:PREFIX;MWDIVID:PREFIX"')
 MEDIAWIKI_HIDDEN_CATEGORIES = config.get('mediawiki', 'hidden_categories').split(',')
 MEDIAWIKI_SHOW_UNPUBLISHED = config.getboolean('mediawiki', 'show_unpublished')
 MEDIAWIKI_DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
